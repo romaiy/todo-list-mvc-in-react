04c51c8ef63b6cdd2da7ae2730c88837064f9651
romai <sokolovgit256@gamil.com>
2023-03-06 19:46:01 +0600

    first commit

---
+++ src/Todolist/TodolistForm/TodolistFormModel/index.js
@@ -0,0 +1,18 @@
+const TodolistFormModal = {
+    isLoading: true,
+    isValid: true,
+    fields: {
+        title: {
+            value: '',
+        },
+        description: {
+            value: '',
+        },
+        type: {
+            value: '',
+            options: '',
+        },
+    },
+};
+
+export default TodolistFormModal;
